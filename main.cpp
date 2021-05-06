2cabfe065ef769f78e91e51345ff1651b104bb12
PHBueno <contato0113@gmail.com>
2021-05-06 00:56:27 -0300

    Adiciona testes para a Classe Empresa

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
 #include "Time.hpp"
+#include "Empresa.hpp"
 #include<iostream>
 using namespace std;
 
 int main(void)
 {
+    Empresa Nacional("Nacional");
+
     Time Internacional("Internacional", "Profissional");
+    Time Gremio("Grêmio", "Profissional");
+    Time Sao_Jose("São José", "Base");
+
+    
 
     Internacional.adiciona_jogador("Pedro", "Profissional", "Brasileiro", 7,10,2000, 3100.00);
     Internacional.adiciona_jogador("Glauco", "Profissional", "Brasileiro", 7,10,1997, 3100.00);
 
-    Internacional.relatorio_geral();
-    Internacional.relatorio_financeiro();
+    Gremio.adiciona_jogador("Paulo", "Profissional", "Brasileiro", 7,10,1997, 3100.00);
+    Sao_Jose.adiciona_jogador("Mariano", "Base", "Brasileiro", 7,11,1997, 3100.00);
+
+    Nacional.adiciona_time(Internacional);
+    Nacional.adiciona_time(Sao_Jose);
+
+    // Nacional.time_na_Empresa(Internacional);
+
+    Nacional.consulta_jogador("Gilberto");
 }
